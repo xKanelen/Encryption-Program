9125c7d8ae675508fd47abf4aedb67be2642dacb
xKanelen <malinionutpricop@gmail.com>
2024-12-02 18:50:43 +0200

    First commit

---
+++ Encryption Program.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+print(f"chars: {chars}")
+print(f"key  : {key}")
