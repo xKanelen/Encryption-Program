92f000f6eefb3529ed3a2f7b5ec2176663de641c
xKanelen <malinionutpricop@gmail.com>
2024-12-03 15:10:13 +0200

    Small update

--- Encryption Program.py
+++ Encryption Program.py
@@ -7,5 +7,29 @@ key = chars.copy()
 
 random.shuffle(key)
 
-print(f"chars: {chars}")
-print(f"key  : {key}")
+#print(f"chars: {chars}")
+#print(f"key  : {key}\n")
+
+#ENCRYPT
+
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Original message: {plain_text}")
+print(f"Encrypted message: {cipher_text}\n")
+
+#DECRYPT
+
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"Encrypted message: {cipher_text}")
+print(f"Original message: {plain_text}")
